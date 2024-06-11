c5907dfe65db29ab4aa13ad717a2c68a9cca3b85
ksbleek <kyle.s.bleeker@gmail.com>
2024-06-11 08:05:04 -0500

    finished pytest

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,17 @@
 class GuessingGame():
-    # Write your code here
+    def __init__(self, answer):
+        self.answer = answer
+    
+        self.temp_game = False
+
+    def guess(self, user_guess):
+        if user_guess > self.answer:
+            return "high"
+        elif user_guess == self.answer:
+            self.temp_game = True
+            return "correct"
+        elif user_guess < self.answer:
+            return "low"
+    def solved(self):
+        return self.temp_game
     pass
